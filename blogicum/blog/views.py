7496a46e25aa717119622ac94405b7aca87d0a12
CyderJedi <rizhykc@gmail.com>
2024-11-06 15:58:44 +0300

    fix views

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -13,11 +13,11 @@ def index(request):
 def post_detail(request, id):
 
     template = 'blog/detail.html'
-    post = get_object_or_404(
+    post_list = get_object_or_404(
         q_post(),
         pk=id,
     )
-    context = {'posts': post}
+    context = {'post': post_list}
     return render(request, template, context)
 
 
@@ -29,7 +29,8 @@ def category_posts(request, category_slug):
         slug=category_slug,
     )
     post_list = (
-        q_post().filter(category__slug=category_slug)
+        q_post()
+        .filter(category__slug=category_slug)
         .order_by('-pub_date')[:10]
     )
     context = {'category': category, 'post_list': post_list}
